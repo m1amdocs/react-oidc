9a844014793ba190755afc5d427e3f38aeac0e9a
David Gurr <davidgur@amdocs.com>
2022-04-29 12:11:27 -0400

    LAUNCH-7736: added rollup + tested build

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+// see https://dev.to/siddharthvenkatesh/component-library-setup-with-react-typescript-and-rollup-onj
+
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import json from "@rollup/plugin-json";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+const packageJson = require('./package.json');
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        name: '@marketone/react-oidc'
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      json({
+        compact: true
+      }),
+      typescript({ tsconfig: './tsconfig.json' }),
+      terser()
+    ]
+  }
+]
